bb09a6fb8b767c7af83b585e4d26bd0529d6cf25
Wilder Cossio <w.cossio.r@gmail.com>
2019-02-12 01:23:01 -0400

    prueba creada, si un numero no es Buzz retornar falso,fallando

--- test/fizzbuzz_test.js
+++ test/fizzbuzz_test.js
@@ -7,9 +7,16 @@ describe('FIZZ BUZZ', function(){
         resultFizz = fizzbuzz.ItisFizz(number);
         expect(false).equal(resultFizz);
     });
+    
     it('al enviar un numero que es divisible por 3, deberia retornar verdadero', function(){
         number = 6;
         resultFizz = fizzbuzz.ItisFizz(number);
         expect(true).equal(resultFizz);
+    });
+
+    it('al enviar un numero que NO es divisible por 5, deberia retornar falso',function(){
+        number = 8;
+        resultBuzz = fizzbuzz.ItisBuzz(number);
+        expect(false).equal(resultBuzz);
     });
 });
