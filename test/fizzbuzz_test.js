5d9cb837ea0390f6c475152ac6be7cecea6a389b
Wilder Cossio <w.cossio.r@gmail.com>
2019-02-12 01:12:44 -0400

    prueba falla , inportado archivo fizzbuzz.js e llamando metodo ItisFizz

--- test/fizzbuzz_test.js
+++ test/fizzbuzz_test.js
@@ -1,8 +1,10 @@
 const expect = require('chai').expect;
+const fizzbuzz = require('../src/fizzbuzz')
 
 describe('FIZZ BUZZ', function(){
     it('al enviar un numero que NO es divisible por 3, deberia retornar falso', function(){
-        
-        expect(true).equal(true);
+        number = 1;
+        resultFizz = fizzbuzz.ItisFizz(number);
+        expect(false).equal(resultFizz);
     });
 });
